e07dadaafd914d248346fe6c18733b21bab04273
Damian Kotulski <kotulskidamian@gmail.com>
2017-04-08 02:32:36 +0200

    #21 dodano wstepne modele danych wygenerowane z uzyciem Entity Frameworka, dodano jeden kontroler w celu przeprowadzenia testow

---
+++ SmartShop/SmartShopWebApp/Controllers/cashiersController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using SmartShopWebApp.Models;
+
+namespace SmartShopWebApp.Controllers
+{
+    public class cashiersController : ApiController
+    {
+        private smartshopEntities db = new smartshopEntities();
+
+        // GET: api/cashiers
+        public IQueryable<cashier> Getcashiers()
+        {
+            return db.cashiers;
+        }
+
+        // GET: api/cashiers/5
+        [ResponseType(typeof(cashier))]
+        public IHttpActionResult Getcashier(int id)
+        {
+            cashier cashier = db.cashiers.Find(id);
+            if (cashier == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cashier);
+        }
+
+        // PUT: api/cashiers/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putcashier(int id, cashier cashier)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != cashier.idcashiers)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(cashier).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!cashierExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/cashiers
+        [ResponseType(typeof(cashier))]
+        public IHttpActionResult Postcashier(cashier cashier)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.cashiers.Add(cashier);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = cashier.idcashiers }, cashier);
+        }
+
+        // DELETE: api/cashiers/5
+        [ResponseType(typeof(cashier))]
+        public IHttpActionResult Deletecashier(int id)
+        {
+            cashier cashier = db.cashiers.Find(id);
+            if (cashier == null)
+            {
+                return NotFound();
+            }
+
+            db.cashiers.Remove(cashier);
+            db.SaveChanges();
+
+            return Ok(cashier);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool cashierExists(int id)
+        {
+            return db.cashiers.Count(e => e.idcashiers == id) > 0;
+        }
+    }
+}
